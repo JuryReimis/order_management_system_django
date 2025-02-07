774df5638c034a01161194daff02c12f286925f7
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-02-07 21:29:48 +0300

    Добавлена модель товара

---
+++ order_management_system_django/carte/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Dish(models.Model):
+
+    title = models.CharField(
+        max_length=150,
+        blank=False,
+        null=False,
+        verbose_name="Название блюда"
+    )
+
+    price = models.PositiveIntegerField(
+        blank=False,
+        verbose_name="Цена"
+    )
+
+    class Meta:
+        verbose_name = "Блюдо"
+        verbose_name_plural = "Блюда"
