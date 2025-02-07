7c42bb52a0487d7ffe64c70aaa828553fc088432
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-02-07 21:30:00 +0300

    Добавлена модель заказа

---
+++ order_management_system_django/orders/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class Order(models.Model):
+
+    PENDING = 0
+    READY = 1
+    PAID = 2
+
+    STATUS = [
+        (PENDING, "В ожидании"),
+        (READY, "Готов"),
+        (PAID, "Оплачено")
+    ]
+
+    table_number = models.IntegerField(
+        blank=False,
+        verbose_name="Номер стола"
+    )
+
+    items = models.ManyToManyField(
+        to='carte.Dish',
+        blank=False,
+        related_name="orders",
+        verbose_name="Заказанные позиции"
+    )
+
+    status = models.IntegerField(
+        choices=STATUS,
+        verbose_name="Статус заказа"
+    )
+
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
