5dd0a75001827c2b9f5ff55f87042d3d9ce91de0
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-02-10 16:19:52 +0300

    Создана модель для записей изменений цены блюда

--- order_management_system_django/carte/models.py
+++ order_management_system_django/carte/models.py
@@ -11,8 +11,10 @@ class Dish(models.Model):
         verbose_name="Название блюда"
     )
 
-    price = models.PositiveIntegerField(
-        blank=False,
+    price = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        default=0,
         verbose_name="Цена"
     )
 
@@ -22,3 +24,32 @@ class Dish(models.Model):
     class Meta:
         verbose_name = "Блюдо"
         verbose_name_plural = "Блюда"
+
+
+class DishPriceChanges(models.Model):
+
+    dish = models.ForeignKey(
+        to='carte.Dish',
+        on_delete=models.CASCADE,
+        verbose_name="Блюдо"
+    )
+
+    new_price = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        default=0,
+        verbose_name="Новая цена"
+    )
+
+    changed = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата изменения цены"
+    )
+
+    def __str__(self):
+        return f'Изменение цены для {self.dish}'
+
+    class Meta:
+        verbose_name = "Изменение цен"
+        verbose_name_plural = "Изменения цен"
+        ordering = ['-changed']
