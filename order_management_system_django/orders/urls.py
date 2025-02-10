dc6bc543375099070b012fba665f7a830730b650
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-02-10 15:11:09 +0300

    Добавлен маршрут для просмотра детальной информации о заказе

--- order_management_system_django/orders/urls.py
+++ order_management_system_django/orders/urls.py
@@ -7,4 +7,5 @@ app_name = "orders"
 urlpatterns = [
     path('create-new-order/', views.CreateNewOrderView.as_view(), name="create_order"),
     path('all-orders/', views.GetAllOrdersView.as_view(), name="get_all_orders"),
+    path('order/<int:pk>/', views.OrderDetailView.as_view(), name="order-detail"),
 ]
