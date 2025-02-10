63983f649d0f250d10d7774b5191a16579679a0e
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-02-10 15:09:59 +0300

    Написаны views для добавления заказа, отображения списка всех заказов и детальной информации об одном заказе.

--- order_management_system_django/orders/views.py
+++ order_management_system_django/orders/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from orders.forms import CreateNewOrderForm
+from orders.models import Order
+
+
+class CreateNewOrderView(generic.CreateView):
+    template_name = 'orders/add-new-order.html'
+    form_class = CreateNewOrderForm
+    success_url = reverse_lazy('orders:create_order')
+
+
+class GetAllOrdersView(generic.ListView):
+    paginate_by = 10
+    context_object_name = 'orders'
+    template_name = 'orders/orders-list.html'
+
+    def get_queryset(self):
+        orders = Order.objects.order_by('-created')
+        return orders
+
+
+class OrderDetailView(generic.DetailView):
+    model = Order
+    template_name = 'orders/order-detail.html'
