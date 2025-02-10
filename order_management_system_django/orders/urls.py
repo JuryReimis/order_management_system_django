b673384e5c93d3ef273eaca6416f4ee206faf554
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-02-10 14:43:03 +0300

    Добавлены маршруты для приложения orders

--- order_management_system_django/orders/urls.py
+++ order_management_system_django/orders/urls.py
@@ -1,6 +1,10 @@
+from django.urls import path
+
+from orders import views
 
 app_name = "orders"
 
 urlpatterns = [
-
+    path('create-new-order/', views.CreateNewOrderView.as_view(), name="create_order"),
+    path('all-orders/', views.GetAllOrdersView.as_view(), name="get_all_orders"),
 ]
