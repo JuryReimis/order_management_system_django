febdffd66be929c490f5772b89b1d69676bf5be4
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-02-10 14:42:29 +0300

    Add __str__ method

--- order_management_system_django/carte/models.py
+++ order_management_system_django/carte/models.py
@@ -16,6 +16,9 @@ class Dish(models.Model):
         verbose_name="Цена"
     )
 
+    def __str__(self):
+        return f'{self.title} за {self.price} Р'
+
     class Meta:
         verbose_name = "Блюдо"
         verbose_name_plural = "Блюда"
