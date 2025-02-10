ce3c298cbec7d326bf8389ccd537b0c280310a02
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-02-10 13:28:20 +0300

    Написаны представления для отображения списка блюд и добавления нового блюда

--- order_management_system_django/carte/views.py
+++ order_management_system_django/carte/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from carte.forms import AddNewDishForm
+from carte.models import Dish
+
+
+class AddNewDishView(generic.CreateView):
+    template_name = 'carte/create-new-dish.html'
+    form_class = AddNewDishForm
+    success_url = reverse_lazy('carte:create_dish')
+
+
+class GetAllDishesView(generic.ListView):
+    paginate_by = 10
+    context_object_name = 'dishes'
+    template_name = 'carte/dish-list.html'
+
+    def get_queryset(self):
+        dishes = Dish.objects.order_by('-price')
+        return dishes
+
