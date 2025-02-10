4840be92084ba7e25be7e9c578fe70506430ec3c
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-02-10 13:27:27 +0300

    Update urls.py

--- order_management_system_django/carte/urls.py
+++ order_management_system_django/carte/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
+from carte import views
+
 app_name = "carte"
 
 urlpatterns = [
-    path(''),
+    path('add-new-dish/', views.AddNewDishView.as_view(), name="create_dish"),
+    path('all-dishes/', views.GetAllDishesView.as_view(), name='get_all_dishes')
 ]
