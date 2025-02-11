ffefc2aed90a83a030cef4c47ee85cd0273e7d31
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-02-11 17:49:54 +0300

    Добавлен маршрут

--- order_management_system_django/carte/urls.py
+++ order_management_system_django/carte/urls.py
@@ -6,5 +6,6 @@ app_name = "carte"
 
 urlpatterns = [
     path('add-new-dish/', views.AddNewDishView.as_view(), name="create_dish"),
+    path('dishes/<int:pk>/', views.DishDetailView.as_view(), name="dish_detail"),
     path('all-dishes/', views.GetAllDishesView.as_view(), name='get_all_dishes')
 ]
