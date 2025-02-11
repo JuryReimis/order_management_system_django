215e6c1f8544a6e84ce93943f7e4b3cce58eef33
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-02-11 15:22:11 +0300

    Добавлен маршрут для поиска по заказам

--- order_management_system_django/orders/urls.py
+++ order_management_system_django/orders/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('all-orders/', views.GetAllOrdersView.as_view(), name="get_all_orders"),
     path('order/<int:pk>/', views.OrderDetailView.as_view(), name="order_detail"),
     path('order/<int:pk>/delete/', views.OrderDeleteView.as_view(), name="order_delete"),
-    path('order/<int:pk>/change-status', views.OrderChangeStatusView.as_view(), name="order_change_status")
+    path('order/<int:pk>/change-status', views.OrderChangeStatusView.as_view(), name="order_change_status"),
+    path('orders/search/', views.OrderSearchView.as_view(), name="order_search")
 ]
