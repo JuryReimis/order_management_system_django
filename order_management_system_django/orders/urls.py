d6864a1460edbadcfcae8c8c658f89384f050424
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-02-11 13:16:39 +0300

    Добавлен маршрут для изменения статуса заказа

--- order_management_system_django/orders/urls.py
+++ order_management_system_django/orders/urls.py
@@ -7,6 +7,7 @@ app_name = "orders"
 urlpatterns = [
     path('create-new-order/', views.CreateNewOrderView.as_view(), name="create_order"),
     path('all-orders/', views.GetAllOrdersView.as_view(), name="get_all_orders"),
-    path('order/<int:pk>/', views.OrderDetailView.as_view(), name="order-detail"),
-    path('order/<int:pk>/delete/', views.OrderDeleteView.as_view(), name="order-delete")
+    path('order/<int:pk>/', views.OrderDetailView.as_view(), name="order_detail"),
+    path('order/<int:pk>/delete/', views.OrderDeleteView.as_view(), name="order_delete"),
+    path('order/<int:pk>/change-status', views.OrderChangeStatusView.as_view(), name="order_change_status")
 ]
