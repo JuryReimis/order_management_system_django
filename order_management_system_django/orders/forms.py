e9537472bf2dfafbb65d09a3662ffc3cdd78a71d
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-02-12 00:46:39 +0300

    Добавлена форма для внесения изменений в items модели

--- order_management_system_django/orders/forms.py
+++ order_management_system_django/orders/forms.py
@@ -1,10 +1,17 @@
 from django import forms
+from django.db import IntegrityError
 
 from orders.models import Order
 
 
 class CreateNewOrderForm(forms.ModelForm):
-
     class Meta:
         model = Order
         fields = ['table_number', 'items']
+
+
+class UpdateOrderItemsForm(forms.ModelForm):
+
+    class Meta:
+        model = Order
+        fields = ['items']
