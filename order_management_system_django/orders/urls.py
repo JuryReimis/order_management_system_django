f0d36eb4b444543cc279b0b0e94958e8e6f27b42
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-02-13 15:17:53 +0300

    Update urls.py

--- order_management_system_django/orders/urls.py
+++ order_management_system_django/orders/urls.py
@@ -7,9 +7,10 @@ app_name = "orders"
 urlpatterns = [
     path('create-new-order/', views.CreateNewOrderView.as_view(), name="create_order"),
     path('all-orders/', views.GetAllOrdersView.as_view(), name="get_all_orders"),
-    path('orders/<int:pk>/', views.OrderDetailView.as_view(), name="order_detail"),
-    path('orders/<int:pk>/delete/', views.OrderDeleteView.as_view(), name="order_delete"),
-    path('orders/<int:pk>/change-status/', views.OrderChangeStatusView.as_view(), name="order_change_status"),
-    path('orders/<int:pk>/change-items/', views.OrderChangeItemsView.as_view(), name="change-order-items"),
-    path('orders/search/', views.OrderSearchView.as_view(), name="order_search")
+    path('<int:pk>/', views.OrderDetailView.as_view(), name="order_detail"),
+    path('<int:pk>/delete/', views.OrderDeleteView.as_view(), name="order_delete"),
+    path('<int:pk>/change-status/', views.OrderChangeStatusView.as_view(), name="order_change_status"),
+    path('<int:pk>/change-items/', views.OrderChangeItemsView.as_view(), name="change-order-items"),
+    path('search/', views.OrderSearchView.as_view(), name="order_search"),
+    path('stats/', views.GetOrdersStatsView.as_view(), name="orders_stats"),
 ]
