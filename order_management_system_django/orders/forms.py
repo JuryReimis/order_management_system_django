dc4a7075fc05a5b8cd4ea4d2f72f20056e4308cc
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-02-13 14:31:29 +0300

    Форма для фиксации временного промежутка

--- order_management_system_django/orders/forms.py
+++ order_management_system_django/orders/forms.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
 from django import forms
-from django.db import IntegrityError
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils import timezone
 
 from orders.models import Order, OrderItems
 
@@ -23,3 +26,32 @@ class UpdateQuantityForm(forms.ModelForm):
     class Meta:
         model = OrderItems
         fields = ['dish_id', 'quantity']
+
+
+class DateRangeForm(forms.Form):
+    start_date = forms.DateTimeField(
+        label="Начальная дата",
+        widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        validators=[
+            MinValueValidator(limit_value=timezone.make_aware(datetime(2000, 1, 1))),
+            MaxValueValidator(limit_value=timezone.now())
+        ]
+    )
+    end_date = forms.DateTimeField(
+        label="Конечная дата",
+        widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        validators=[
+            MinValueValidator(limit_value=timezone.make_aware(datetime(2000, 1, 1))),
+            MaxValueValidator(limit_value=timezone.now())
+        ]
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        start_date = cleaned_data.get('start_date')
+        end_date = cleaned_data.get('end_date')
+
+        if start_date and end_date and start_date > end_date:
+            raise forms.ValidationError("Начальная дата должна быть меньше или равна конечной дате.")
+
+        return cleaned_data
