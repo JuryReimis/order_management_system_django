fd4e7f7d61c1e77cfa695e0bd8d35901e34d9e5c
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-02-14 14:58:15 +0300

    Update urls.py

--- order_management_system_django/carte/urls.py
+++ order_management_system_django/carte/urls.py
@@ -7,5 +7,6 @@ app_name = "carte"
 urlpatterns = [
     path('add-new-dish/', views.AddNewDishView.as_view(), name="create_dish"),
     path('dishes/<int:pk>/', views.DishDetailView.as_view(), name="dish_detail"),
-    path('all-dishes/', views.GetAllDishesView.as_view(), name='get_all_dishes')
+    path('dishes/<int:pk>/delete/', views.DishDeleteView.as_view(), name="dish_delete"),
+    path('all-dishes/', views.GetAllDishesView.as_view(), name="get_all_dishes")
 ]
