23369043870a6cb7ac06b0fb664ad1f652a86ffc
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-02-14 13:48:00 +0300

    Подключено приложение rest framework

--- order_management_system_django/order_management_system_django/settings.py
+++ order_management_system_django/order_management_system_django/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
+    'rest_framework',
     'orders.apps.OrdersConfig',
     'carte.apps.CarteConfig',
 ]
