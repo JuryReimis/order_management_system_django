d561c01f706fad5371afe0d9ddd73e3fd412b5ce
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-02-15 16:33:31 +0300

    Добавлены описания

--- order_management_system_django/orders/views.py
+++ order_management_system_django/orders/views.py
@@ -26,11 +26,15 @@ from orders.services.update_order import UpdateOrderService
 
 
 class CreateNewOrderView(generic.CreateView):
+    r"""Create представление для заказов"""
+
     template_name = 'orders/add-new-order.html'
     form_class = CreateNewOrderForm
     success_url = reverse_lazy('orders:create_order')
 
     def get(self, request, *args, **kwargs):
+        r"""Метод get возвращает страницу с шаблоном и дополнительный контекст
+        для формирования списка доступных блюд"""
         dishes = Dish.objects.order_by('-price')
         self.extra_context = {
             'dishes': dishes
@@ -38,14 +42,20 @@ class CreateNewOrderView(generic.CreateView):
         return super().get(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
+        r"""Метод обрабатывает POST-запрос, инициирует работу сервисов по созданию нового заказа"""
         items = request.POST.getlist('items')
         if items:
+            # Если список items не пустой
             order_repository = OrderRepository
             update_order_items_repository = UpdateOrderItemsRepository
             dish_price_repository = DishPriceRepository
-            dto = OrderItemsDTO(
+
+            # Формирование словаря, с доступом к количеству блюд по id
+            items_quantity_dict = {int(item): int(request.POST.get(f'item_{item}-quantity', 1)) for item in items}
+
+            dto = OrderItemsDTO(  # Объект для передачи данных в сервис
                 order_id=None,
-                items_quantity_dict={int(item): int(request.POST.get(f'item_{item}-quantity', 1)) for item in items},
+                items_quantity_dict=items_quantity_dict,
                 last_update=None,
                 table_number=request.POST.get('table_number')
             )
@@ -61,6 +71,7 @@ class CreateNewOrderView(generic.CreateView):
             else:
                 messages.success(request, "Заказ успешно создан")
         else:
+            # Если нет items - выдать ошибку и вернуть страницу создания еще раз
             messages.error(request=request, message="Заказ не может быть пустым", extra_tags='danger')
         return self.get(request, *args, **kwargs)
 
@@ -73,6 +84,7 @@ class CreateNewOrderView(generic.CreateView):
 
 
 class GetAllOrdersView(generic.ListView):
+    r"""Получение списка всех заказов. Стандартный ListView"""
     paginate_by = 9
     context_object_name = 'orders'
     template_name = 'orders/orders-list.html'
@@ -83,6 +95,7 @@ class GetAllOrdersView(generic.ListView):
 
 
 class OrderDetailView(GetOrderDetailMixin, generic.DetailView):
+    r"""Детальная информация про заказ."""
     model = Order
     template_name = 'orders/order-detail.html'
     context_object_name = 'order'
@@ -100,6 +113,7 @@ class OrderDetailView(GetOrderDetailMixin, generic.DetailView):
 
 
 class OrderDeleteView(generic.DeleteView):
+    r"""Удаление заказа. Стандартное представление."""
     success_url = reverse_lazy('orders:get_all_orders')
 
     def get_object(self, queryset=None):
@@ -108,20 +122,27 @@ class OrderDeleteView(generic.DeleteView):
 
 
 class OrderChangeItemsView(View):
+    r"""Представление для изменения состава заказа"""
     form_class = UpdateOrderItemsForm
     template_name = 'orders/update-order-items.html'
     model = Order
 
     def get(self, request, *args, **kwargs):
+        r"""Обработка Get-запроса"""
         order = Order.objects.get(pk=kwargs['pk'])
+
+        # Получаем блюда, невключенные в заказ
         dishes = list(Dish.objects.exclude(pk__in=order.items.all().values_list('id', flat=True)))
+
         context = self.get_context_data(order=order, items=dishes)
         return render(request, self.template_name, context=context)
 
     def post(self, request, *args, **kwargs):
+        r"""Обработка POST запроса."""
         order_id = kwargs['pk']
         items = request.POST.getlist('items')
         if order_id is not None and items is not None:
+            # Если в запросе присутствуют и order_id и выбраны блюда для включения в заказ
             dto = OrderItemsDTO(
                 order_id=order_id,
                 items_quantity_dict={int(item): int(request.POST.get(f'item_{item}-quantity', 1)) for item in items},
@@ -147,12 +168,16 @@ class OrderChangeItemsView(View):
         return redirect(self.get_success_url())
 
     def get_context_data(self, order: Order, items: List[Dish], **kwargs):
+        r"""Получаем контекст для отображения в шаблоне"""
         order_items = order.order_items.all()
+
+        # Формируем формсет для массового включения форм в шаблон
         formset = self.get_formset(order_items)
-        form_dict = {}
+
+        form_dict = {}  # Словарь для доступа к форме по dish_id
         for form in formset:
             dish_id = form.initial['dish_id']
-            form.prefix = f"item_{dish_id}"
+            form.prefix = f"item_{dish_id}"  # Меняем префикс для поиска по POST data осле отправки формы
             form_dict[dish_id] = form
         context = {
             'order': order,
@@ -170,15 +195,19 @@ class OrderChangeItemsView(View):
         )
 
     def get_success_url(self):
+        r"""Перенаправление на страницу с детальной информацией"""
         return reverse_lazy('orders:order_detail', kwargs={'pk': self.kwargs.get('pk')})
 
 
 class OrderChangeStatusView(View):
+    r"""Представление для смены статуса заказа"""
 
     def post(self, request, *args, **kwargs):
         try:
             obj = self.get_object()
             new_status = Order.PENDING
+
+            # Проверка какая кнопка была нажата
             if 'ready' in request.POST:
                 new_status = Order.READY
             elif 'paid' in request.POST:
@@ -199,16 +228,20 @@ class OrderChangeStatusView(View):
 
 
 class OrderSearchView(GetOrdersListMixin, generic.ListView):
+    r"""Поиск по заказам"""
     paginate_by = 9
     context_object_name = 'orders'
     template_name = 'orders/orders-list.html'
 
 
 class GetOrdersStatsView(View):
+    r"""Представление для получения статистики за определенный период"""
+
     template_name = 'orders/orders_stats.html'
     form: DateRangeForm = DateRangeForm
 
     def get(self, request, *args, **kwargs):
+        r"""Формирует страницу с формой для поиска по периоду времени"""
         form = self.form(request.GET)
         context = {
             'form': form
@@ -216,6 +249,8 @@ class GetOrdersStatsView(View):
 
         start_date_str = request.GET.get('start_date')
         end_date_str = request.GET.get('end_date')
+
+        # Если введены данные в форме поиска - формирует контекст статистики
 
         if start_date_str and end_date_str:
             if form.is_valid():
