138daf4aa39ffb1c4ab7ded56eaeddc57dd1a011
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-02-15 14:35:02 +0300

    Переадресация с пустого url

--- order_management_system_django/order_management_system_django/urls.py
+++ order_management_system_django/order_management_system_django/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
+from django.views.generic import RedirectView
 
 from order_management_system_django import settings
 
@@ -9,6 +10,7 @@ urlpatterns = [
     path('', include('carte.api.v1.urls')),
     path('', include('orders.api.v1.urls')),
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url=reverse_lazy('orders:create_order')))
 ]
 
 if settings.DEBUG is True:
