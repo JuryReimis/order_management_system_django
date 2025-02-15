09a30cb7621182e5d56f6461b6a5095f77f05c14
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-02-15 13:03:49 +0300

    Сохранение экземпляра вынесено в отдельный метод. Добавлен метод для обновления статуса. Мелкие доработки

--- order_management_system_django/orders/repositories/order.py
+++ order_management_system_django/orders/repositories/order.py
@@ -1,4 +1,7 @@
 from decimal import Decimal
+
+from django.core.exceptions import ValidationError
+from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from orders.dto.order import OrderDTO
 from orders.dto.order_items import OrderItemsDTO
@@ -19,7 +22,7 @@ class OrderRepository:
     def _get_order(self):
         order_id = self._order_items_dto.order_id
         if order_id is not None:
-            return Order.objects.get(pk=order_id)
+            return get_object_or_404(Order, pk=order_id)
         table_number = self._order_items_dto.table_number
         if table_number is None:
             raise ValueError("Произошла ошибка, в обработку не предано ни номера стола, ни id заказа")
@@ -33,7 +36,16 @@ class OrderRepository:
         order = self._order
         order.total_price = new_total_price
         order.updated = last_update
-        order.save()
+
+    def _update_status(self, new_status):
+        order = self._order
+        if new_status in list(map(lambda status_tuple: status_tuple[0], Order.STATUS)):
+            order.status = new_status
+        else:
+            raise ValidationError(f"У заказа не может быть статуса {new_status}")
+
+    def _order_save(self):
+        self._order.save()
 
     def get_order_data(self) -> OrderDTO:
         if self._order:
@@ -49,5 +61,13 @@ class OrderRepository:
             return dto
         raise ValueError("Нет объекта")
 
-    def update_order(self, total_price: Decimal):
-        self._update_total_price(total_price)
+    def update_order(self, total_price: Decimal | None, new_status: int | None):
+        if new_status:
+            self._update_status(new_status)
+        if total_price:
+            self._update_total_price(total_price)
+        if total_price is None and new_status is None:
+            raise ValidationError(f"Запрос должен содержать данные, для изменения: "
+                                  f"items - список словарей с ключами dish_id и quantity "
+                                  f"или status в разрешенных значениях {Order.STATUS}")
+        self._order_save()
